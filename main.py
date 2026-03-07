f4af0d2773e85c2ef0ae1f24d777ebcd35a401a6
赵奕诚 <89493052+Zhao-Yicheng@users.noreply.github.com>
2026-03-08 00:16:49 +0800

    更新 main.py
    适配了新的 API

--- main.py
+++ main.py
@@ -11,19 +11,21 @@ def get_display_width(s):
     return width
 
 data = m.request_list(input('请输入歌名或歌手: '))
-for idx, i in enumerate(data, 1):
-    name = i['name']
+for i in range(len(data)):
+    song = data[i]
+    name = song['name']
     padding = 20 - get_display_width(name)
-    print(f"{idx}. {name}{' ' * max(0, padding)}{i['singer']}")
+    print(f"{i+1}. {name}{' ' * max(0, padding)}{song['singer']}")
 
 choice = int(input('请输入编号下载: '))
 if 1 <= choice <= len(data):
     song = data[choice - 1]
     filename = f"{song['name']} - {song['singer']}.mp3"
+    music_url = m.request_music_url(song['link'])
     print(f"开始下载: {filename}")
-    
+    response = requests.get(music_url,headers=m.headers)
     with open(filename, 'wb') as f:
-        f.write(requests.get(m.request_url(song['link'])).content)
+        f.write(response.content)
     print(f"已下载: {filename}")
 else:
     print("编号无效")
